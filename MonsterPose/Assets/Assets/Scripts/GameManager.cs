e3f5ab05a3609eeedaf74237a16b03e02106d61c
Nguyễn Hoàng Quế Hòa <99721296+QueHoa@users.noreply.github.com>
2023-11-10 17:17:15 +0700

    update

--- MonsterPose/Assets/Assets/Scripts/GameManager.cs
+++ MonsterPose/Assets/Assets/Scripts/GameManager.cs
@@ -75,7 +75,7 @@ public class GameManager : MonoBehaviour
         Application.targetFrameRate = 60;
         Input.multiTouchEnabled = false;
         Instance = this;
-        //PlayerPrefs.SetInt("levelsUnlocked", 1);
+        //PlayerPrefs.SetInt("levelsUnlocked", 100);
         if (!PlayerPrefs.HasKey("levelsUnlocked"))
         {
             PlayerPrefs.SetInt("levelsUnlocked", 1);
